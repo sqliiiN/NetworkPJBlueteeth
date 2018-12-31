f06e14ffbce0dacc16e6e6532e418c19755d5b0e
sweetLychee <15307130407@fudan.edu.cn>
2018-12-31 19:13:59 +0800

    Update src/ble/index/index.js

--- src/ble/index/index.js
+++ src/ble/index/index.js
@@ -1,5 +1,15 @@
 const app = getApp()
 
+// 需要用到的 UUIDs
+var reserveduuid = {
+  // 自定义蓝牙uuid
+  target_uuid: "00B14C5D-AD30-8262-895F-CBFC46278D9C",
+  // 电池
+  battery_uuid: "00002A19-0000-1000-8000-00805F9B34FB",
+  // 温湿度传感器
+  sensor_uuid: "0000FFF6-0000-1000-8000-00805F9B34FB",
+};
+
 function inArray(arr, key, val) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i][key] === val) {
@@ -7,6 +17,21 @@ function inArray(arr, key, val) {
     }
   }
   return -1;
+}
+
+// 16进制字符串转10进制
+function hex2dec(hex, start) {
+  let dec = 0;
+  let base = 1;
+  for (let i = 1; i >= 0; i--) {
+    if (i < 1) base *= 16;
+    if (hex[start + i] >= '0' && hex[start + i] <= '9') {
+      dec = dec + (hex[start + i] - '0') * base;
+    } else {
+      dec = dec + (hex[start + i] - 'a') * base;
+    }
+  }
+  return dec.toString();
 }
 
 // ArrayBuffer转16进度字符串
@@ -20,21 +45,26 @@ function ab2hex(buffer) {
   return hexArr.join('');
 }
 
+
 Page({
   data: {
     devices: [],
     connected: false,
     chs: [],
-  },
 
-  // UUIDs of different services
-  reserveduuid:{
-    battery: "2A19",
-    sensor: "FFF6",
-    rename: "FFF7",
+    // 蓝牙设备传输到手机上的信息
+    // 电池电量
+    batterylev: null,
+    // 温度整数和小数位
+    temperature_high: null,
+    temperature_low: null,
+    // 湿度整数和小数位
+    humidity_high: null,
+    humidity_low: null,
   },
 
   /** 
+   * 
    * openBluetoothAdapter: 开启蓝牙适配器后，搜索附近蓝牙设备.
    *    1. 蓝牙适配器开启成功, 开始搜寻附近的蓝牙外围设备;
    *    2. 蓝牙适配器开启失败, 监听蓝牙适配器的变化;
@@ -60,6 +90,7 @@ Page({
   },
 
   /** 
+   * 
    * getBluetoothAdapterState: 获取本机蓝牙适配器状态.
    *    1. discovering (正在搜索设备) -> 监听寻找到新设备的事件
    *    2. available (蓝牙适配器是否可用) -> 开始搜寻附近的蓝牙外围设备
@@ -78,6 +109,7 @@ Page({
   },
 
   /** 
+   * 
    * startBluetoothDevicesDiscovery: 开始搜寻附近的蓝牙外围设备.
    *    1. 如果已经在搜索, 直接返回
    *    2. 如果没有, 将标志设置成true, 并开始搜寻附近的蓝牙外围设备
@@ -97,6 +129,7 @@ Page({
   },
 
   /** 
+   * 
    * stopBluetoothDevicesDiscovery: 停止搜寻附近的蓝牙外围设备.
    */
   stopBluetoothDevicesDiscovery() {
@@ -104,6 +137,7 @@ Page({
   },
 
   /** 
+   * 
    * onBluetoothDeviceFound: 监听寻找到新设备的事件.
    * 存储并显示所有的设备信息, 同步改变视图
    */
@@ -111,7 +145,7 @@ Page({
     wx.onBluetoothDeviceFound((res) => {
       res.devices.forEach(device => {
         //if (!device.name && !device.localName) {
-          //return
+        //return
         //}
         const foundDevices = this.data.devices
         const idx = inArray(foundDevices, 'deviceId', device.deviceId)
@@ -127,6 +161,7 @@ Page({
   },
 
   /** 
+   * 
    * createBLEConnection: 建立BLE连接.
    *    1. 获取设备ID与设备名name
    *    2. 如果连接建立成功, 调用getBLEDeviceServices获取服务
@@ -144,8 +179,6 @@ Page({
           name,
           deviceId,
         })
-        // TODO: 判断是否为"00B14C5D-AD30-8262-895F-CBFC46278D9C"
-        // 是则通过特别的规则获取服务
         this.getBLEDeviceServices(deviceId)
       }
     })
@@ -153,6 +186,7 @@ Page({
   },
 
   /** 
+   * 
    * closeBLEConnection: 关闭BLE连接.
    */
   closeBLEConnection() {
@@ -167,6 +201,7 @@ Page({
   },
 
   /** 
+   * 
    * getBLEDeviceServices: 获取蓝牙设备所有服务.
    *    如果成功获得设备服务, 
    *    调用getBLEDeviceCharacteristics获取服务的特征值
@@ -178,7 +213,6 @@ Page({
         for (let i = 0; i < res.services.length; i++) {
           if (res.services[i].isPrimary) {
             this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid)
-            //return
           }
         }
       }
@@ -186,6 +220,7 @@ Page({
   },
 
   /** 
+   * 
    * getBLEDeviceCharacteristics: 获取服务的特征值并监听特征值变化事件.
    *    调用API时需要设备ID和服务ID
    *    1. 如果调用成功, 调用getBLEDeviceCharacteristics获取服务的特征值
@@ -208,7 +243,7 @@ Page({
               characteristicId: item.uuid,
             })
           }
-          if (item.properties.write) {
+          /**if (item.properties.write) {
             this.setData({
               canWrite: true
             })
@@ -216,7 +251,7 @@ Page({
             this._serviceId = serviceId
             this._characteristicId = item.uuid
             this.writeBLECharacteristicValue()
-          }
+          }*/
           if (item.properties.notify || item.properties.indicate) {
             wx.notifyBLECharacteristicValueChange({
               deviceId,
@@ -240,10 +275,44 @@ Page({
           uuid: characteristic.characteristicId,
           value: ab2hex(characteristic.value)
         }
+        // 获取蓝牙设备信息
+        //
+        if (deviceId === reserveduuid["target_uuid"]) {
+          // 电池信息
+          //
+          if (data[`chs[${this.data.chs.length}]`].uuid === reserveduuid.battery_uuid) {
+            data.batterylev = hex2dec(data[`chs[${this.data.chs.length}]`].value, 0)
+          }
+          // 传感器信息
+          //
+          else if (data[`chs[${this.data.chs.length}]`].uuid === reserveduuid.sensor_uuid) {
+            data.temperature_low = hex2dec(data[`chs[${this.data.chs.length}]`].value, 6)
+            data.temperature_high = hex2dec(data[`chs[${this.data.chs.length}]`].value, 4)
+            data.humidity_low = hex2dec(data[`chs[${this.data.chs.length}]`].value, 2)
+            data.humidity_high = hex2dec(data[`chs[${this.data.chs.length}]`].value, 0)
+          }
+        }
       } else {
         data[`chs[${idx}]`] = {
           uuid: characteristic.characteristicId,
           value: ab2hex(characteristic.value)
+        }
+        // 获取蓝牙设备信息
+        //
+        if (deviceId === reserveduuid["target_uuid"]) {
+          // 电池信息
+          //
+          if (data[`chs[${idx}]`].uuid === reserveduuid.battery_uuid) {
+            data.batterylev = hex2dec(data[`chs[${idx}]`].value, 0)
+          }
+          // 传感器信息
+          //
+          else if (data[`chs[${idx}]`].uuid === reserveduuid.sensor_uuid) {
+            data.temperature_low = hex2dec(data[`chs[${idx}]`].value, 6)
+            data.temperature_high = hex2dec(data[`chs[${idx}]`].value, 4)
+            data.humidity_low = hex2dec(data[`chs[${idx}]`].value, 2)
+            data.humidity_high = hex2dec(data[`chs[${idx}]`].value, 0)
+          }
         }
       }
       this.setData(data)
@@ -251,6 +320,7 @@ Page({
   },
 
   /** 
+   * 
    * closeBluetoothAdapter: 关闭适配器.
    */
   closeBluetoothAdapter() {
